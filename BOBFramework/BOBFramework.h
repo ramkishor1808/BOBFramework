6b7d010d1b18c7466e65b37b1c74cc131a61a969
Ram Kishor Pal <ramkishor.p@ebix.com>
2021-05-26 15:42:11 +0530

    first commit

---
+++ BOBFramework/BOBFramework.h
@@ -0,0 +1,18 @@
+//
+//  BOBFramework.h
+//  BOBFramework
+//
+//  Created by Venkat Naresh on 26/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BOBFramework.
+FOUNDATION_EXPORT double BOBFrameworkVersionNumber;
+
+//! Project version string for BOBFramework.
+FOUNDATION_EXPORT const unsigned char BOBFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BOBFramework/PublicHeader.h>
+
+
